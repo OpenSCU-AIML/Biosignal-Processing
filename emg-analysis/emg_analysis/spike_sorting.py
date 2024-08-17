afbf92052e7adfc857cb790fe9a86f6a7aac27f4
rukasuamarike <lukeamlicke@gmail.com>
2024-08-16 21:23:24 -0400

    spike sorting WIP

--- emg-analysis/emg_analysis/spike_sorting.py
+++ emg-analysis/emg_analysis/spike_sorting.py
@@ -27,11 +27,12 @@ channelArrays = sensors.sens[0].npChans()
 vis = visual.ChanPlot(channelArrays)
 
 # %%
+# DISPLAY
 vis.chanplot(channelArrays)
 
 
 # %%
-
+# hi and low bandpass filter
 from scipy.signal import butter, lfilter
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -41,39 +42,75 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     b, a = butter(order, [low, high], btype='band')
     return b, a
 
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
 
+# %%
+# robust detection threshold
+import numpy as np
+# median abs dev
+def threshMAD(arr):
+    mean = np.mean(arr)
+    dev = [np.absolute(x-mean)/0.6745 for x in arr]
+    return np.median(dev)
+def threshIdx(arr, thresh):
+    return [idx for idx,x in enumerate(arr) if (x <= -thresh)]
+def detectSpikes(y,Fs):
+    th = threshMAD(y)
+    threshIdx(y, thresh)
+
 
 # %%
-fs = float(sensors.sens[0].sr.strip().split(' ')[0])
+# LOAD 
 nchan = sensors.sens[0].nchan//2
 channels = channelArrays
-# %%
-# Sample rate and desired cutoff frequencies (in Hz).
-# fs = 2222.0
-lowcut = 500.0
-highcut = 1111.0
-# %%
-# Filter a noisy signal.
-T = 0.1
-idx = int(5.8*fs)
-nsamples = int(T * fs) 
-t = np.linspace(0, T, nsamples, endpoint=False) # Generate the time vector 
-cur = (idx,idx+nsamples)
-data = [channels[i][cur[0]:cur[1]] for i in range(nchan)]
-for i in range(nchan):
-    y = butter_bandpass_filter(data[i], lowcut, highcut, fs, order=6)
-    plt.subplot(4, 1, i+1)
-    plt.plot(t,y,label=f'ch {i+1}' )
+# FILTER: Sample rate and desired cutoff frequencies (in Hz).
+fs = float(sensors.sens[0].sr.strip().split(' ')[0])
+lowcut = 20.0
+highcut = 150.0 # 50% fmax
+filtered = [butter_bandpass_filter(chan, lowcut, highcut, fs, order=3) for chan in channels]
 
-plt.xlabel('time (seconds)')
-plt.grid(True)
-plt.legend(loc='upper left')
+# %%
+def samplebytime(channels, s=0.1):
+    T = s
+    idx = int(5.8*fs)
+    nsamples = int(T * fs) 
+    t = np.linspace(0, T, nsamples, endpoint=False)
+    cur = (idx,idx+nsamples)
+    data = [channels[i][cur[0]:cur[1]] for i in range(nchan)]
+    print(f"{T} seconds | samples: {nsamples}")
+    return data
+# %%
+vis.chanplot(filtered)
 
-plt.show()
+
+# %% 
+# THRESHOLD:
+thresh = [threshMAD(chan) for chan in filtered]
+spikes = [threshIdx(filtered[i],thresh[i]) for i in range(len(filtered))]
+
+
+# %%
+print(len(filtered))
+# %%
+vis.tickplot(filtered, spikes)
+# %%
+type(ticks[3][0])
+# %%
+sample = filtered[0][0:500]
+# tt = threshIdx(sample,threshMAD(sample))
+th = np.ones(len(sample)) * -1 * threshMAD(sample)
+print(len(sample),len(th))
+comp = np.c_[sample,th]
+plt.plot(range(len(sample)),comp)
+
+# %%
+mad = MAD(data[0])
+rmad = rMAD(data[0])
+# %%
+print(mad)
+print(rmad)
 
 # %%
